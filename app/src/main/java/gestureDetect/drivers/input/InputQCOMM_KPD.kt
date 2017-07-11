8f4adc0d028f4fc4e6d4a3e405fffc6c0baf8618
SnowCatPDA <snowcat@vpro.ru>
2017-07-11 19:21:18 +0500

    ADD Xiaomi volume UP button

--- app/src/main/java/gestureDetect/drivers/input/InputQCOMM_KPD.kt
+++ app/src/main/java/gestureDetect/drivers/input/InputQCOMM_KPD.kt
@@ -9,7 +9,7 @@ Qualcomm keys
 open class InputQCOMM_KPD(gesture: GestureDetect) : InputHandler(gesture)
 {
     override fun onDetect(name:String):Boolean {
-        if (!arrayOf("qpnp_pon",  "gpio-keys")
+        if (!arrayOf("qpnp_pon",  "gpio-keys", "hbtp_vm")
                 .contains(name.toLowerCase())) return false
 
         return onDetectKeys(name)
