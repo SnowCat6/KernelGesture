031d27d269a6d83ea6294ca5f4df70e4a3f65eb4
SnowCatPDA <snowcat@vpro.ru>
2017-10-16 13:16:31 +0500

    OPTIMIZE

--- app/src/main/java/ru/vpro/kernelgesture/SettingsActivity.kt
+++ app/src/main/java/ru/vpro/kernelgesture/SettingsActivity.kt
@@ -4,6 +4,7 @@ package ru.vpro.kernelgesture
 import SuperSU.ShellSU
 import android.app.AlertDialog
 import android.app.FragmentManager
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
