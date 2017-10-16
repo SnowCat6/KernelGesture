4c90983eaf54ab35ba39b05bac77b71fbe72a418
SnowCatPDA <snowcat@vpro.ru>
2017-10-16 22:16:50 +0500

    RECODE ReadEvent binary

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -1,10 +1,9 @@
-APP := EventReader
 
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE     := $(APP)
-LOCAL_SRC_FILES  := input.h-labels.h $(APP).c
+LOCAL_MODULE     := EventReader
+LOCAL_SRC_FILES  := $(LOCAL_MODULE).c
 
 include $(BUILD_EXECUTABLE)
--- app/src/main/cpp/EventReader.c
+++ app/src/main/cpp/EventReader.c
@@ -5,7 +5,14 @@
 #include <string.h>
 #include <stdio.h>
 #include<pthread.h>
+#include <stdint.h>
+#include <dirent.h>
+#include <sys/inotify.h>
+#include <sys/poll.h>
+#include <errno.h>
+#include <time.h>
 
+#define MAX_DEVICE_INPUT 10
 #define LABEL(constant) { #constant, constant }
 #define LABEL_END { NULL, -1 }
 
@@ -30,72 +37,90 @@ static const char *get_label(const struct label *labels, int value)
     return labels->name;
 }
 
+void onEvent(int nfds, struct input_event event);
+
 struct ThreadArg{
     const char* device;
+    int fd;
+    struct input_event last_event;
 };
-static struct ThreadArg args[20];
+static nfds_t nfds = 0;
+static struct pollfd ufds[MAX_DEVICE_INPUT];
+static struct ThreadArg thisEvents[MAX_DEVICE_INPUT];
 
 void* readInput(void *arg);
+void sig_handler(int signo);
 
 int main(int argc, char **argv)
 {
     setbuf(stdout, NULL);
     setbuf(stderr, NULL);
+    signal(SIGINT, sig_handler);
 
-    printf("Event reader started!\n");
+    printf("Event reader started\n");
 
-    int i;
-    for (i = 1; i < argc && i < 20; i++)
+    for (int i = 1; i < argc && nfds < MAX_DEVICE_INPUT; i++)
     {
-        args[i].device = argv[i];
+        thisEvents[nfds].device = argv[i];
+        thisEvents[nfds].fd = open(thisEvents[nfds].device, O_RDONLY);
+        if (thisEvents[nfds].fd <= 0) {
+            fprintf(stdout, "Error open: %s\n", thisEvents[nfds].device);
+            continue;
+        }
 
-        pthread_t tid = 0;
-        int err = pthread_create(&tid, NULL, &readInput, &args[i]);
-        if (err != 0) printf("can't create thread :[%s]\n", strerror(err));
+        ufds[nfds].fd = thisEvents[nfds].fd;
+        ufds[nfds].events = POLLIN;
+
+        fprintf(stdout, "Opened: %s\n", thisEvents[nfds].device);
+        ++nfds;
     }
 
-    while(getchar() != ' ');
+    int bOK = 1;
+    struct input_event event;
+
+    while(bOK)
+    {
+        poll(ufds, nfds, -1);
+        for(int i = 0; i < nfds; i++)
+        {
+            if (ufds[i].revents == 0) continue;
+            if((ufds[i].revents & POLLIN) == 0) continue;
+
+            bOK = read(ufds[i].fd, &event, sizeof(event)) == sizeof(event);
+            if (!bOK) break;
+
+            onEvent(i, event);
+        }
+    }
+
+//    while(getchar() != ' ');
 
     return 0;
 }
-void* readInput(void *arg)
+
+void onEvent(int nfds, struct input_event event)
 {
-    struct ThreadArg* input = (struct ThreadArg*)arg;
+    if (event.type != EV_KEY) return;
+    struct ThreadArg *thisEvent = &thisEvents[nfds];
 
-    int fd = 0;
-    // Write a key to the keyboard buffer
-    if((fd = open(input->device, O_RDONLY)) <= 0){
-        fprintf(stdout, "Error open: %s\n", input->device);
-        return NULL;
+    struct timeval diff;
+    timersub(&event.time, &thisEvent->last_event.time, &diff);
+
+    fprintf(stderr, "[%8ld.%06ld] %s: %-12.12s %-20.20s  %s\n",
+            diff.tv_sec, diff.tv_usec,
+            thisEvent->device,
+            "EV_KEY",
+            get_label(key_labels, event.code),
+            get_label(key_value_labels, event.value));
+
+    memcpy(&thisEvent->last_event, &event, sizeof(event));
+}
+
+void sig_handler(int signo)
+{
+    printf("Event reader exit\n");
+    for(int i = 0; i < nfds; i++){
+        close(thisEvents->fd);
     }
-
-    fprintf(stdout, "Opened: %s\n", input->device);
-
-    struct input_event event, last_event;
-    memset(&last_event, 0, sizeof(last_event));
-
-    while(1)
-    {
-        memset(&event, 0, sizeof(event));
-        ssize_t nRead = read(fd, &event, sizeof(event));
-        if (nRead == (ssize_t)-1){
-            if (errno == EINTR) continue;
-            printf("Failed read input device:%s\n", input->device);
-            break;
-        }
-        if (event.type != EV_KEY) continue;
-
-        struct timeval diff;
-        timersub(&event.time, &last_event.time, &diff);
-
-        fprintf(stderr, "[%8ld.%06ld] %s: %-12.12s %-20.20s  %s\n",
-               diff.tv_sec, diff.tv_usec,
-               input->device,
-               "EV_KEY",
-               get_label(key_labels, event.code),
-               get_label(key_value_labels, event.value));
-
-        memcpy(&last_event, &event, sizeof(last_event));
-    }
-    close(fd);
+    exit(0);
 }
