d2679c8ec8f840e19dc6a70a397b704ee9be08fc
SnowCatPDA <snowcat@vpro.ru>
2017-10-21 21:48:21 +0500

    ADD copy to clipboard

--- app/src/main/java/ru/vpro/kernelgesture/tools/adapter/ReAdapter.kt
+++ app/src/main/java/ru/vpro/kernelgesture/tools/adapter/ReAdapter.kt
@@ -1,14 +1,26 @@
 package ru.vpro.kernelgesture.tools.adapter
 
+import android.support.design.widget.Snackbar
+import org.inowave.planning.common.tools.toClipboard
 import org.inowave.planning.ui.common.adapter.*
+import ru.vpro.kernelgesture.R
 import ru.vpro.kernelgesture.tools.HeaderString
 import ru.vpro.kernelgesture.tools.TwoString
 
-class ReAdapter(items : List<Any>? = null) : RecyclerAdapterT<Any>(items)
+class ReAdapter(items : List<Any>? = null)
+    : RecyclerAdapterT<Any>(items)
 {
     init {
         bindClass(Any::class.java,          HolderItemAny::class.java)
         bindClass(HeaderString::class.java, HolderItemHeader::class.java)
         bindClass(TwoString::class.java,    HolderItemTwoString::class.java)
+
+        onClickItem { view, index, item ->
+
+            val context = view.context
+            context.toClipboard(item.toString())
+            Snackbar.make(view, context.getString(R.string.ui_snack_clipboard), Snackbar.LENGTH_SHORT)
+                    .show()
+        }
     }
 }
